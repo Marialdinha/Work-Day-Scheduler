c408877f75ed58bc1ba7b8a3fdd458a63d74f457
Marialdinha <marialda@bellsouth.net>
2023-05-27 13:51:44 -0500

    Added checkCurrentHour funcion

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,83 @@
+// our vqriables
+var eachHour = [];
+var todaysDate;
+var currrentHour;
+
+
+// Initialization
+function init(){
+
+  // get todays's date
+  todaysDate =  dayjs().format("dddd, MMMM D YYYY");
+  $('#currentDay').text(todaysDate);
+  // get current hour
+  checkCurrentHour();
+  // popuate calendar with existing events
+  retrieveEvents();
+  }
+
+
+ // popuate calendar with existing events
+  function retrieveEvents(){
+
+      // eachHour = [];
+  // var toDoEvents = JSON.parse(localStorage.getItem("allEvents")) || [];
+  
+  // if (toDoEvents.length > 0) {
+  
+  //   var hour = 9;
+  //   for(index = 0; index < toDoEvents.length; index++){
+  //     $(`#hour-${hour}`).text(toDoEvents[index]);
+  //     hour++;
+  // }
+     
+  // }
+  }
+
+  // function saveEvents(event){
+
+  // }
+
+
+  // Function to set color to each hour
+ function checkCurrentHour(){
+  currrentHour = new dayjs().format("HH");
+
+  $(".time-block").each(function(){
+      calendarHour = $(this).attr("id").substring(5)
+      if (calendarHour === currrentHour){
+         $(this).addClass("present")
+      }else if(calendarHour < currrentHour){
+         $(this).addClass("past") 
+      }else {
+        $(this).addClass("future")
+      }
+  })
+ }
+
+ function setEventListeners(){
+
+  
+}
+
+
+$(document).ready(function(){
+
+  init();
+  setEventListeners();
+})
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-function init(){
-var hour9 = localStorage.getItem("hour9");
-$("#hour9").text(hour9);
-}
 
 
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -27,4 +96,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
