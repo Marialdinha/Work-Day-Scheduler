ebb22de10d88b1f49e3731e863184a34502dc20b
Marialdinha <marialda@bellsouth.net>
2023-05-26 22:57:52 -0500

    Added all hours to calendar

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+function init(){
+var hour9 = localStorage.getItem("hour9");
+$("#hour9").text(hour9);
+}
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
