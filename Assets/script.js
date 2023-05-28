574a9e98e34bae4b0a088d9f62d76d70ed299749
Marialdinha <marialda@bellsouth.net>
2023-05-28 11:29:18 -0500

    saveEvents almos working corectly

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,12 @@
-// our vqriables
+// our variables
 var todaysDate;
 var currrentHour;
 
 
 // Initialization
 function init(){
+
+  $("#appointment-added").hide();
 
   // get todays's date
   todaysDate = new dayjs().format("dddd, MMMM D YYYY");
@@ -19,30 +21,31 @@ function init(){
  // popuate calendar with existing events in localstorage
   function retrieveEvents(){
 
+   console.log(localStorage);
   if (localStorage !== null) {
     for(index = 0; index < localStorage.length; index++){
         var keyHour = localStorage.key(index);
-        var mssage = localStorage.getItem(index);
-        $(`#hour-${keyHour}`).text(mssage);
-  }}else{
+        var message = localStorage.getItem(keyHour);
+        $(`#hour-${keyHour}`).children("textarea").val(message);
+    }}
+  }
 
-  }
-  }
 
   // Function to save events to localstorage
   function saveEvents(buttonHour){
-    console.log(buttonHour);
-    var message =  $(`#hour-${buttonHour}`).children("textarea").value;
-    console.log(message); 
+
+    var message =  $(`#hour-${buttonHour}`).children("textarea").val();
 
     if  (message === null){
-        localStorage.removeItem(buttonHour);
+        localStorage.removeItem(buttonHour);  //<-- here not working
     }else {
         localStorage.setItem(buttonHour, message);
     }
           
         // Refresh page wth new events
         retrieveEvents();
+
+        $("#appointment-added").show();
   }
 
 
@@ -81,20 +84,3 @@ $(document).ready(function(){
     init();
     setEventListeners();
 })
-
-
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-// });
